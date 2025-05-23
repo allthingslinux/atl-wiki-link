efa00fb9b374ea6e1b9d26bee183e739883db093
Scott <scott@allthingslinux.org>
2025-05-22 23:21:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ ALLOWED_ROLE_IDS = list(map(int, os.getenv("ALLOWED_ROLE_IDS", "").split(",")))
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.guilds = True
+intents.members = True
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
 def create_token():
@@ -30,11 +32,9 @@ async def on_ready():
     purge_old_links.start()
     print(f"Logged in as {bot.user}!")
 
+# Updated: Anyone can run this command
 @bot.command()
 async def verify(ctx: commands.Context):
-    if not any(role.id in ALLOWED_ROLE_IDS for role in ctx.author.roles):
-        return await ctx.send("You do not have permission to run this command.")
-    
     token = create_token()
     user_id = ctx.author.id
     now = datetime.utcnow()
@@ -46,9 +46,26 @@ async def verify(ctx: commands.Context):
             conn.commit()
 
     link = f"{VERIFICATION_URL}?token={token}"
-    await ctx.author.send(f"Click this link to verify your MediaWiki account:
-{link}")
+    await ctx.author.send(f"Click this link to verify your MediaWiki account:\n{link}")
     await ctx.send("I've sent you a DM with the verification link!")
+
+# New: Only allowed roles can run this command
+@bot.command(name="check_verified")
+async def check_verified(ctx: commands.Context):
+    if not any(role.id in ALLOWED_ROLE_IDS for role in ctx.author.roles):
+        return await ctx.send("You do not have permission to run this command.")
+
+    with db_conn() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT discord_id FROM links WHERE verified = TRUE")
+            results = cur.fetchall()
+
+    if not results:
+        await ctx.send("No users have been verified yet.")
+        return
+
+    verified_users = "\n".join([f"<@{row[0]}>" for row in results])
+    await ctx.send(f"Verified users:\n{verified_users}")
 
 @tasks.loop(minutes=30)
 async def purge_old_links():
