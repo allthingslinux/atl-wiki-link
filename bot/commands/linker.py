fc3356ca3391a745eb659eff46149dbf54c4cb2e
madomado <madonuko@outlook.com>
2025-08-04 15:19:08 +0800

    chore(linker): compile the regex

--- bot/commands/linker.py
+++ bot/commands/linker.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 import re
 
-RE_WIKI_LINK_IN_MESSAGE = r"\[\[([^\n\[\]#<|{}_�:][^\n\[\]#<|{}_�]*)\]\](?=([^`]|```([^`]|`{1,2}([^`]|$))+```|``([^`]|`([^`]|$))+``|`[^`]+`)*$)"
+RE_WIKI_LINK_IN_MESSAGE = re.compile(r"\[\[([^\n\[\]#<|{}_�:][^\n\[\]#<|{}_�]*)\]\](?=([^`]|```([^`]|`{1,2}([^`]|$))+```|``([^`]|`([^`]|$))+``|`[^`]+`)*$)")
 
 class AutoLinker(commands.Cog):
     """Automatically reply to a message with links to atl.wiki articles if it contains a properly formatted wikilink"""
@@ -17,13 +17,12 @@ class AutoLinker(commands.Cog):
         if message.author.bot:
             return
 
-        matches = re.findall(RE_WIKI_LINK_IN_MESSAGE, message.content)
+        matches = RE_WIKI_LINK_IN_MESSAGE.findall(message.content)
 
         if (len(matches) == 0):
             return
 
-        matches = [match[0] for match in matches]
-        links = [f'[atl.wiki/{match}](https://atl.wiki/{match.replace(" ", "_")})' for match in matches]
+        links = [f'[atl.wiki/{match[0]}](https://atl.wiki/{match[0].replace(" ", "_")})' for match in matches]
         
         await message.reply(content=', '.join(links))
 
